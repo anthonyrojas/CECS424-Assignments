6b059421f6464a2a7641cfb41918ccc3219f5e3a
anthonyrojas <anthony.rojas095@gmail.com>
2016-12-06 11:22:11 -0800

    Update imaginary_friends.pl

--- imaginary_friends.pl
+++ imaginary_friends.pl
@@ -2,6 +2,7 @@ imaginary_friend(grizzly_bear).
 imaginary_friend(moose).
 imaginary_friend(seal).
 imaginary_friend(zebra).
+
 adventure(circus).
 adventure(rock_band).
 adventure(spaceship).
@@ -13,42 +14,45 @@ solve :-
     imaginary_friend(RalphFriend),
     imaginary_friend(WinnieFriend),
     all_different([JoanneFriend, LouFriend, RalphFriend, WinnieFriend]),
+    
     adventure(JoanneAdventure),
     adventure(LouAdventure),
     adventure(RalphAdventure),
     adventure(WinnieAdventure),
     all_different([JoanneAdventure, LouAdventure, RalphAdventure, WinnieAdventure]),
-    Triples=[ [Joanne, JoanneFriend, JoanneAdventure],
-              [Lou, LouFriend, LouAdventure],
-              [Ralph, RalphFriend, RalphAdventure],
-              [Winnie, WinnieFriend, WinnieAdventure] ],
-    %1. The seal --who isnt a  creation of either Joanne or Lou--
+    
+    Triples=[ [joanne, JoanneFriend, JoanneAdventure],
+              [lou, LouFriend, LouAdventure],
+              [ralph, RalphFriend, RalphAdventure],
+              [winnie, WinnieFriend, WinnieAdventure] 
+            ],
+    %1. The seal (who isnt a  creation of either Joanne or Lou)
     %neither rode to the moon in a spaceship
     %nor took a trip around the world on a magic train
-    \+ member([Joanne, seal, _], Triples),
-    \+ member([Lou, seal, _], Triples),
-    \+ member([_, seal, spaceship], Triples),
+    \+ member([joanne, seal, _], Triples),
+    \+ member([lou, seal, _], Triples),    
+    \+ member([_, seal, spaceship], Triples),    
     \+ member([_, seal, train], Triples),
     %2. Joanne's imaginary friend who's not a grizzly
     %went to the circus
-    \+ member([Joanne, grizzly_bear, _], Triples),
-    member([Joanne, _, circus], Triples),
+    \+ member([joanne, grizzly_bear, _], Triples),
+    member([joanne, _, circus], Triples),    
     \+ member([_, grizzly_bear, circus], Triples),
-    %3. Winnie's imaginary friend is a zebra
-    member([Winnie, zebra, _], Triples),
+    %3. Winnie's imaginary friend is a zebra    
+    member([winnie, zebra, _], Triples),
     %4. The grizzly bear didn't board the spaceship to the moon
     \+ member([_, grizzly_bear, spaceship], Triples),
-    tell(Joanne, JoanneFriend, JoanneAdventure),
-    tell(Lou, LouFriend, LouAdventure),
-    tell(Ralph, RalphFriend, RalphAdventure),
-    tell(Winnie, WinnieFriend, WinnieAdventure).
+    
+    tell(joanne, JoanneFriend, JoanneAdventure),
+    tell(lou, LouFriend, LouAdventure),    
+    tell(ralph, RalphFriend, RalphAdventure),    
+    tell(winnie, WinnieFriend, WinnieAdventure).
 
 all_different([H | T]) :- member(H, T), !, fail.
 all_different([_ | T]) :- all_different(T).
 all_different([_]).
 
-tell(X,Y,Z) :- 
-    write('Test: '),
-    write(X), 
-    write(Y), 
+tell(X, Y, Z) :- 
+    write(X), write(' has the imaginary friend of a '), 
+    write(Y), write(' and they went on/in '), 
     write(Z), nl.
